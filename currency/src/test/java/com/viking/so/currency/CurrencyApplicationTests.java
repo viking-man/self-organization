97627be4835cc503ac2c9904149e0fa27940a43f
湛海（蒋威） <jiangwei@tuya.com>
2020-05-12 20:37:28 +0800

    currency-货币系统

---
+++ currency/src/test/java/com/viking/so/currency/CurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viking.so.currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
